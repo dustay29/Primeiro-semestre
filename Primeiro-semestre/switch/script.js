30f9b7c1f3fee70bbd49700b5ce9d7c05cec9bd1
dustay29 <166452414+dustay29@users.noreply.github.com>
2024-04-08 21:58:01 -0300

    Add files via upload

---
+++ Primeiro-semestre/switch/script.js
@@ -0,0 +1,57 @@
+let number = 0
+let resultado = document.getElementById("divResultado")
+    
+ while ( number <= 100){
+
+number +=
+ resultado.innerHTML += number + "<br>"
+ number++
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// let resultado = document.getElementById("divResultado")
+// console.log (resultado);
+
+// // let nome = prompt ("Nome")
+// // resultado.innerHTML  += "Olá " + nome
+
+// let cont = 10
+// while (cont < 0){
+//     resultado.innerHTML = cont 
+//     cont--
+
+// }
+// // while (cont > 0){
+    //     resultado.innerHTML += cont + " patinhos voltarambr>"
+    
+    // }
+
+
+    //          1
+    // let number = 0
+    // let resultado = document.getElementById("divResultado")
+        
+    //  while ( number < 10){
+    //  resultado.innerHTML += number*number + "<br>"
+    //  number++
+    // }
+    //--------------------------------
+    //            2
+    // let number = 10
+    // let resultado = document.getElementById("divResultado")
+    
+    // while ( number > 0){
+    //     resultado.innerHTML += number + "<br>"
+    //     number--
+    // }
+    
