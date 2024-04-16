147b34c0f7c003541e07997f1c1c2ea745e9b730
dustay29 <166452414+dustay29@users.noreply.github.com>
2024-04-15 21:43:23 -0300

    Add files via upload

---
+++ Primeiro-semestre/switch/aulao.js
@@ -0,0 +1,97 @@
+//          Números em forma decrescente:
+// let number = Number (prompt("Digite um número")) 
+    
+// while ( number >= 1){
+// console.log(number)
+// number--
+// }
+
+//          Números ao quadrado:
+// let i = 0
+
+// while (i <= 10) {
+// console.log (i * i)
+// i++
+// }
+
+//          Soma de números de 1 a 100
+// let soma = 0
+// let i = 1
+
+// while (i <=100) {
+// soma = soma + i
+// i++
+// }
+// console.log(soma)
+
+//          Imprimir a tabuada do 5:
+// let i = 0
+
+// while (i <= 10) {
+//     console.log (i * 5)
+// i++
+// }
+
+//          Imprimir a qualquer tabuada:
+// let tabuada = Number (prompt("Desejas ter o resultado de qual tabuada?"))
+//  let i = 0
+
+// while (i <= 10) {
+//     console.log (i * tabuada)
+//     i++
+//  }
+
+//          Imprima os números ímpares de 1 a 20. 
+//   let i = 1
+//  while (i <= 20) {
+//     if (i %2 !==0)
+//     console.log (i)
+//     i++
+// } 
+
+//          Imprima os números de forma crescente e decrescente
+// let i = 0
+
+// while (i <= 10) {
+//     console.log (i)
+//     i++
+// }
+// let ii = 9
+// while (ii >=1){
+//     console.log (ii)
+//     ii--
+// }
+
+//          Calcule o fatorial de um número dado.
+
+//  let num1 = Number (prompt("Digite um número:"))
+//  let soma = 0
+
+//  while (num1 >= 1 ){
+//     soma += num1 * num1
+//     console.log (num1 * num1)
+//     num1--
+//  }
+//  console.log ("Resultado é "+soma)
+ 
+//          Imprima os números de 1 a 100, substituindo 
+//   os múltiplos de 3 por "Fizz", os múltiplos de 5 por "Buzz" e os múltiplos de ambos por "FizzBuzz".
+
+let i = 1
+
+while (i <= 100){
+
+    console.log(i);
+    i++ (A FAZER)
+
+
+}
+
+
+
+
+
+
+
+
+
