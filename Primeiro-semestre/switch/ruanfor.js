737bd400cdfeb92e607e7775a70a4159adfb1a76
dustay29 <166452414+dustay29@users.noreply.github.com>
2024-04-25 21:53:04 -0300

    Add files via upload

---
+++ Primeiro-semestre/switch/ruanfor.js
@@ -0,0 +1,73 @@
+
+// for (let i = 0;i<=5;i++){
+
+//     console.log ("contador " + i)
+// }
+
+// let soma = 0
+// let numero = 0
+
+// for (let i = 0; i < 5; i++){
+//     numero = Number (prompt("Digite um número:"))
+//     if (numero > 7 )
+//     soma += numero;
+// }
+// alert (soma)
+
+// for (let i = 0; i <= 200; i++){
+//     if (i % 2 == 0){
+//      console.log (i)
+//     }
+// }
+
+// soma = 0
+// for (let i = 10; i <=20; i++){
+// soma += i
+// }
+// console.log (soma)
+
+// let i = Number (prompt("Digite um número:"))
+// let soma = 0
+
+// for (let contador = 0; contador <= i; contador++){
+//     soma += contador
+// }
+// alert (soma)
+
+// let i = Number (prompt("Digite o número de multas que você quer cadastrar:"))
+// soma = 0
+// somapontos = 0
+
+// for (let contador = 0; contador < i; contador++){
+//     let multas = Number (prompt("Digite o valor da sua multa:"))
+//     soma += multas
+//     let pontos = Number (prompt("Digite o valor dos pontos:"))
+//     somapontos += pontos
+// }
+// if(somapontos > 21){
+//     alert ("Você está irregular")
+// }else{
+//     alert ("Você está regular")
+// }
+// if (soma >= 5000){ 
+//     alert ("Você pode comprar um gol 1980")
+// }else{
+//     alert ("Não pode comprar um gol 1980")
+// }
+
+// let numero = 0
+// let soma = 0
+// let div = 0
+// let contador
+
+// for (contador = 0; contador < 5; contador++){
+//     numero = Number (prompt("Digite um número par:"))
+//     if(numero % 2 != 0){
+//         alert ("Um dos números não é par")
+        
+//     }else{
+//         soma+=numero
+//     }
+// }
+// div = soma/contador
+// alert (" A média é " + div)
