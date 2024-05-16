6ce53b097c13edb2221c342a5f0933f95843d654
dustay29 <166452414+dustay29@users.noreply.github.com>
2024-05-15 21:56:14 -0300

    Add files via upload

--- Primeiro-semestre/switch/exerc.js
+++ Primeiro-semestre/switch/exerc.js
@@ -1,40 +1,81 @@
 
-// let notas = [];
-// let nota
-// let maior = 0
-// let menor = 10
-// let soma = 0
-// for (cont = 0;cont < 6;cont++){
-//     nota = Number (prompt("Digite uma nota de 0 a 10"))
-//     notas.push(nota);
-//     soma += nota
-//     if (nota > maior){
-//         maior = nota
+//  let notas = [];
+//  let nota
+//  let maior = 0
+//  let menor = 10
+//  let soma = 0
+//  for (cont = 0;cont < 6;cont++){
+//      nota = Number (prompt("Digite uma nota de 0 a 10"))
+//      notas.push(nota);
+//      soma += nota
+//      if (nota > maior){
+//          maior = nota
+//      }
+//      if (nota < menor){
+//          menor = nota
+//      }
+//  }
+//  let somaFinal = soma - maior - menor
+//  somaFinal = somaFinal / 4
+//  console.log ("Suas notas foram: " + notas)
+//  console.log ("A maior nota foi: " + maior)
+//  console.log ("A menor nota foi: " + menor)
+//  console.log ("A sua média final foi: " + somaFinal)
+
+
+//2
+// let inteiros = [];
+// let num
+// let numImpar = []
+// let numPar = []
+
+// for (let cont = 0; cont < 5; cont++){
+//     num = Number (prompt("Digite um número inteiro: "))
+//     inteiros.push(num);
+//     if (num % 2 != 0){
+//      numImpar.push(num)
 //     }
-//     if (nota < menor){
-//         menor = nota
-//     }
+//     else{
+//     numPar.push(num)
+//    }
 // }
-// let somaFinal = soma - maior - menor
-// somaFinal = somaFinal / 4
-// console.log (notas)
-// console.log (maior)
-// console.log (menor)
-// console.log (somaFinal)
 
-let inteiros = [];
-let num
-let numImpar = 0
-let numPar = 0
+// console.log (numPar)
+// console.log (numImpar)
 
-for (let cont = 0; cont < 5; cont++){
-    num = Number (prompt("Digite 5 números inteiros: "))
-    inteiros.push(num);
-    if (num => num % 2 != 0){
-        numImpar = num
-    }else{
-        numPar = num
+
+////3
+// let fila = []
+
+// while (fila.length < 5){
+// let i = Number (prompt("Digite 1 para ENTRAR ou 2 para SAIR: "))    
+// if (i == 1){
+//     let nome = (prompt("Qual o seu nome? "))
+//     fila.push(nome)
+//  }
+// else if (i == 2){
+//     fila.shift()
+// }
+// else {
+//     alert ("Comando não encontrado!")
+// }
+// }
+// alert ("A fila está cheia, temos cinco pessoas nela (" + fila + ")")
+
+//4
+
+let anos = []
+for (let i = 2010; i <= 2019; i++){
+    let a = (prompt("Teve alistamento em "+ i + " (S or N)? "))
+    if (a == "S"){
+    anos.push ("S")
+    }
+    if (a == "N"){
+    anos.push ("N")
     }
 }
-console.log (inteiros)
+alert (a)
+
+
+
 
