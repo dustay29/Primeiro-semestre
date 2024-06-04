c437b7da364c3750b9e6d0d3df4cff547cf648e0
dustay29 <166452414+dustay29@users.noreply.github.com>
2024-06-03 21:56:50 -0300

    Add files via upload

---
+++ Primeiro-semestre/switch/atv/a.js
@@ -0,0 +1,5 @@
+let input = document.getElementById("id_nome")
+let form = document.getElementById (".formulario")
+let label = document.getElementById ("label")
+let labelAll = document.querySelectorAll ("label")
+console.log(input)
