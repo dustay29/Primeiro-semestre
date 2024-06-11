ad27668002e922b21e61962d2be3ac8d395ac3b8
dustay29 <166452414+dustay29@users.noreply.github.com>
2024-06-10 21:12:02 -0300

    Add files via upload

--- Primeiro-semestre/switch/eventoss/imc.js
+++ Primeiro-semestre/switch/eventoss/imc.js
@@ -1,20 +1,28 @@
-let d = 
 
-function enviarFormulario(evento){
-    evento.preventDefault()
-
-    const formulario = document.querySelector("form")
-
-    const dadosForm = new FormData(formulario)
-
-    const valorNome = dadosForm.get('nome')
-    const valorAltura = dadosForm.get('altura')
-    const valorPeso = dadosForm.get('peso')
-    const valorProfissao = dadosForm.get('profissao')
-
-    let alturaDobrada = valorAltura * valorAltura
-    let imc = valorPeso / alturaDobrada
-    alert (valorNome + "\n" + valorAltura + "\n" + valorPeso + "\n" + valorProfissao + "\n" + imc)
-    
-
+function enviarFormulario(even){
+    even.preventDefault()
+    let form = document.getElementById("form")
+    let formdata = new FormData(form)
+    let valorNome = formdata.get('nome')
+    let valorAltura = Number (formdata.get('altura'))
+    let valorPeso = Number (formdata.get('peso'))
+    let valorProfissao = formdata.get('profissao')
+    let p = document.getElementById ('response')
+    console.log (valorAltura)
+    console.log (valorPeso)
+    let imc = valorPeso/(valorAltura*valorAltura)
+    let coisas = ""
+    let opt = ["MAGREZA","NORMAL","SOBREPESO","OBESIDADE","OBESIDADE GRAVE"]
+    if (imc <18.5){
+        coisas=opt[0]
+    }else if(imc >=18.5 && imc < 25){
+        coisas=opt[1]
+    }else if(imc >=25 && imc < 30){
+        coisas=opt[2]
+    }else if(imc >=30 && imc < 40){
+        coisas=opt[3]
+    }else{
+        coisas=opt [4]
+    }
+    p.innerHTML = (`<p>Nome: ${valorNome}\nProfissão: ${valorProfissao}<p>Resultado: ${imc} , ${coisas}`)
 }
